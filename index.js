f7acfc4b6e159fc155b9b9ad21663ae00d0d6f3b
Julien <julien.casse@digica.sh>
2017-08-16 16:54:35 +0200

    15

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ exports.handler = (event, context, callback) => {
     // currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: 'The time in Los Angeles is: TEST ',
+        body: 'The time in Los Angeles is: TEST2 ',
     });
 };
