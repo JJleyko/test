8d6a7bdbf4f4a62f9c7b9c51df40a8877de8fd8c
Julien <julien.casse@digica.sh>
2017-08-17 10:15:11 +0200

    20

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// var time = require('time');
+var time = require('time');
 exports.handler = (event, context, callback) => {
-    // var currentTime = new time.Date(); 
-    // currentTime.setTimezone("America/Los_Angeles");
+    var currentTime = new time.Date(); 
+    currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
-        body: {response:'The time in Los Angeles is: TEST2 '},
+        body: {response:'The time in Los Angeles is: TEST2 ' + currentTime.toString()},
     });
 };
